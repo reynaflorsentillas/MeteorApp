e8fb071a926d2791c759abc8d4074c266d0a0b94
reynaflorsentillas <reynaflorsentillas@ymail.com>
2016-04-22 14:00:40 +0800

    Remove Auto Publish and Insecure Publish and Subscription Folder Organization

---
+++ client/subscriptions.js
@@ -0,0 +1,3 @@
+/**
+ * Created by ReynaflorSentillas on 4/22/2016.
+ */
---
+++ server/startup.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ReynaflorSentillas on 4/22/2016.
+ */
+Meteor.startup(function () {
+    // code to run on server at startup
+});
